d7185756d88bb29d754e0ed3b8b47560fd62c49d
thr828 <592040808@qq.com>
2020-12-06 16:02:21 +0800

    电商PI

---
+++ FakeXiecheng.API/FakeXiecheng.API/Models/TouristRoute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FakeXiecheng.API.Models
+{
+    /// <summary>
+    /// 旅游路线
+    /// </summary>
+    public class TouristRoute
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public double? DiscountPresent { get; set; }
+
+        public DateTime CreateTime { get; set; }
+        public DateTime? UpDateTime { get; set; }
+        public DateTime? DepartureTime { get; set; }
+        public string Features { get; set; }
+        public string Fees { get; set; }
+        public string Notes { get; set; }
+
+        public ICollection<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+    }
+}
